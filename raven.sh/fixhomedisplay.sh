619b937e0a7f2703c2c7acf6e7570475df980fc6
wrdecurtins <william.decurtins@gmail.com>
2022-05-03 13:23:18 -0500

    fix home display

--- raven.sh/fixhomedisplay.sh
+++ raven.sh/fixhomedisplay.sh
@@ -1,2 +1,2 @@
-xrandr --output DP-2-1 --mode 3840x2160 --rate 30 --pos 0x0 --rotate normal
+xrandr --output DP-3-1 --mode 3840x2160 --rate 30 --pos 0x0 --rotate normal
 xrandr --output eDP-1 --pos 3841x1080
