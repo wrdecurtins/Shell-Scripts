f0cac0c983fb7db512fbe7c351307e51e45ce169
wrdecurtins <william.decurtins@gmail.com>
2022-05-13 09:19:05 -0500

    fix fixdisplay

--- raven.sh/fixdisplay.sh
+++ raven.sh/fixdisplay.sh
@@ -1,2 +1,3 @@
-xrandr --output DP-3-3 --mode 3840x2160 --rate 30 --pos 0x0 --rotate normal --left-of DP-3-1
+xrandr --output DP-2-1 --mode 3840x2160 --rate 60
+xrandr --output DP-2-3 --mode 3840x2160 --rate 30 --pos 0x0 --rotate normal --left-of DP-2-1
 xrandr --output eDP-1 --pos 2880x2160
