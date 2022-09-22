2fc40cef852c55aeb319ec88e4dd1cdf6d909d02
wrdecurtins <william.decurtins@gmail.com>
2022-09-22 11:28:01 -0500

    update db1 run command

--- raven.sh/run-db1.sh
+++ raven.sh/run-db1.sh
@@ -1,1 +1,7 @@
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes && docker run --network host -it docker.raven.engineering/atd/dot/db1/db1:latest python3 dot.py -e dev --sim -m 254
+if  [[ "$PWD" != '/home/will/Desktop/db1' ]]; then
+  echo "Changing to db1 directory"
+  cd /home/will/Desktop/db1
+fi
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --network host -it docker.raven.engineering/atd/dot/db1/db1:latest python3 dot.py -e dev --sim -m 254
