3c9511203ff44347c433dbe9420888e714f3c40a
William DeCurtins <william.decurtins@ravenind.com>
2024-04-02 12:13:44 -0500

    fix typo

--- raven.sh/alarm-app.sh
+++ raven.sh/alarm-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if  [[ "$PWD" != '/home/will/Desktop/alarmx-schema' ]]; then
-  echo "Changing to alarmx-schema directory"
-  cd /home/will/Desktop/alarmx-schema
+if  [[ "$PWD" != '/home/will/Desktop/alarm-schema' ]]; then
+  echo "Changing to alarm-schema directory"
+  cd /home/will/Desktop/alarm-schema
 fi
 
 cargo run -p alarming
