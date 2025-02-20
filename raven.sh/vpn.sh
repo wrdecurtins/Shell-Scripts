7ef971f8c0b1b3148c24e81bf2691fbbb56d138b
wrdecurtins <william.decurtins@gmail.com>
2025-02-20 11:07:24 -0600

    update vpn

--- raven.sh/vpn.sh
+++ raven.sh/vpn.sh
@@ -1,1 +1,2 @@
-sudo systemctl start ravenvpn.service
+domain_password=$(sed -En 's/password=?(.*)$/\1/p' /home/will/.domain_credentials)
+sudo openconnect --authgroup "Raven Enhanced VPN" --user r105100 proxy.ravenind.com --key-password $domain_password
